e9ee8918a916987e0c1b3da3b1cc7336cb48b266
Josip Matijas <josip.matijas@atos.net>
2020-01-05 22:48:22 +0100

    Added countdown timer with progressbar. Colors changed.

--- app/src/main/java/com/josipmatijas/wordingrid/MainActivity.java
+++ app/src/main/java/com/josipmatijas/wordingrid/MainActivity.java
@@ -1,15 +1,24 @@
 package com.josipmatijas.wordingrid;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName();
+
+    private ProgressBar mTimerProgressBar;
+    private TextView mTimerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +28,41 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setImageResource(R.drawable.ic_fab_add);
+        fab.setImageResource(R.drawable.ic_fab_play_arrow);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Start a new game?", Snackbar.LENGTH_LONG)
+                        .setAction("Start", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                startNewGame();
+                            }
+                        }).show();
             }
         });
+
+        // timer textview and progressbar
+        mTimerProgressBar = (ProgressBar) findViewById(R.id.progressBar_timer);
+        mTimerTextView = (TextView) findViewById(R.id.textView_timer);
+
+        mTimerProgressBar.setMax(105000);
+    }
+
+    private void startNewGame() {
+        new CountDownTimer(105000, 100) {
+
+            public void onTick(long millisUntilFinished) {
+                mTimerTextView.setText("" + millisUntilFinished / 1000);
+                Log.d(TAG, "millisUntilFinished: " + ((int)millisUntilFinished));
+                mTimerProgressBar.setProgress((int)millisUntilFinished);
+            }
+
+            public void onFinish() {
+                mTimerTextView.setText("time's up!");
+            }
+
+        }.start();
     }
 
     @Override
